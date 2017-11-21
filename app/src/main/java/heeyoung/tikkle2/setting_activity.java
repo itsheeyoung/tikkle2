36b38b985a58479ce8fd11cb686a7e0d97268f3d
itsheeyoung <heeyoung601@naver.com>
2017-11-21 14:59:05 +0900

    Mainactivity변경(근데 잘 안됨) signupactivity변경 longinactivity추가 bene_signup_activity추가 setting_acitivity 아직 안함 supporter_home_activity아직 어려워서 멈춤.

---
+++ app/src/main/java/heeyoung/tikkle2/setting_activity.java
@@ -0,0 +1,13 @@
+package heeyoung.tikkle2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class setting_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting_activity);
+    }
+}
