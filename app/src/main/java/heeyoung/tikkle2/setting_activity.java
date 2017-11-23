731602dea706d3175e50160c39ee8aead3bf842f
dldnjstjrq <dldnjstjrq@naver.com>
2017-11-23 19:25:02 +0900

    기능 조금 추가

--- app/src/main/java/heeyoung/tikkle2/setting_activity.java
+++ app/src/main/java/heeyoung/tikkle2/setting_activity.java
@@ -7,11 +7,9 @@ public class setting_activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_setting_activity);
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_setting_activity);
     }
-
-
 
 
 
